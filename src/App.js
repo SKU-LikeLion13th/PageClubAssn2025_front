c80b878d3b20a9d6b9c136cdb9858ff1ff55a12a
cleame8922 <cleame8922@sungkyul.ac.kr>
2025-01-08 21:36:43 +0900

    UI 디자인

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Fonts from "./components/font_test";
-import './css/style.css'
+import './css/style.css';
+import Footer from './components/Footer';
+import ExcellentClub from './pages/User/ExcellentClub';
 
 function App() {
   return (
-    <div className="App">
-      <Fonts />
+    <div className="flex flex-col min-h-screen App">
+      {/* Main Content */}
+      <div className="flex-grow">
+        <ExcellentClub />
+      </div>
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className='flex justify-center font-Ownglyph_ryurue text-[15px] text-[#d2b48c]'>제41대 함께 채워나갈 우리의 이야기, Page 동아리연합회</div>
+    )
+}
