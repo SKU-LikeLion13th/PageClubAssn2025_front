a9a477916225fbe4ecc0497b4f7860a9373f13fa
minseo121 <kittse1217@gmail.com>
2025-01-10 16:27:03 +0900

    [fix] 간격 조정

--- src/pages/User/Maintest.jsx
+++ src/pages/User/Maintest.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const Maintest = () => {
-    return (
-        <div>
-            main 화면 테스트입니다. <br/>
-            <br/>
-            main 화면 코드 만들어지면 해당 파일을 삭제하셔도 됩니다.<br/>
-            <br/>
-            해당 파일은 삭제하고 routes 폴더에 있는 user.jsx에서 main 임포트 파일 경로만 수정해주세요.<br/><br/>
-            파일 경로는 만든 main 파일과 연결하시면 됩니다.<br/>
-        </div>
-    );
+  return (
+    <div>
+      main 화면 테스트입니다. <br/>
+      <br/>
+      main 화면 코드 만들어지면 해당 파일을 삭제하셔도 됩니다.<br/>
+      <br/>
+      해당 파일은 삭제하고 routes 폴더에 있는 user.jsx에서 main 임포트 파일 경로만 수정해주세요.<br/><br/>
+      파일 경로는 만든 main 파일과 연결하시면 됩니다.<br/>
+    </div>
+  );
 };
 
 export default Maintest;
