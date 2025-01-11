378e8a6b717f5e381911b02dfd1dd6a7def2f96f
Koohyewon <khw6746@naver.com>
2025-01-11 21:09:13 +0900

    [design]loginFailureModal

--- src/pages/User/Login/LoginFailureModal.jsx
+++ src/pages/User/Login/LoginFailureModal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function LoginFailureModal() {
   return (
     <>
-      <div className="w-full h-[calc(100vh-35px)] flex justify-center items-center bg-[#000000]/[.35]">
+      <div className="w-full h-screen flex justify-center items-center bg-[#000000]/[.35]">
         <div className="w-[80%] text-center">
           <div className="relative h-[130px] bg-[#FFFFFF] font-Moneygraphy text-[#996515] border border-[#D2B48C] rounded-lg flex items-center justify-center">
             <span className="absolute top-1 right-2">x</span>
