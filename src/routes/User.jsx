33bcfe1ce1bf6ad8b3cb94d578170b8928aa2999
Koohyewon <khw6746@naver.com>
2025-01-11 20:19:02 +0900

    [add] Login.jsx, [fix] routes/User.jsx

--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import {Route, Routes, useLocation} from "react-router-dom";
-import Main from '../pages/User/Maintest'
-import ExcellentClub from '../pages/User/ExcellentClub/ExcellentClub';
-import RentalItems from '../pages/User/RentalItems';
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Main from "../pages/User/Maintest";
+import ExcellentClub from "../pages/User/ExcellentClub/ExcellentClub";
+import RentalItems from "../pages/User/RentalItems";
+import Login from "../pages/User/Login/Login";
 
 const User = () => {
   return (
     <div>
       <Routes>
-        <Route path = "/" element = {<Main/>}/>
-        <Route path = "/excellentclub" element={<ExcellentClub/>}/>
-        <Route path = "/rentalitems" element={<RentalItems/>}/>
+        <Route path="/" element={<Main />} />
+        <Route path="/excellentclub" element={<ExcellentClub />} />
+        <Route path="/rentalitems" element={<RentalItems />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
