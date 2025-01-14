f6b8d6eac3aa997ebceb464d565fda7afccbef92
minseo121 <kittse1217@gmail.com>
2025-01-15 03:34:11 +0900

    [add]:page 배경 완성

--- src/pages/User/Rental/RentalItems.jsx
+++ src/pages/User/Rental/RentalItems.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Item from "./Item";
+import PageBack from "./PageBack";
 
 const RentalItems = () => {
   return (
     <div className="relative min-h-screen w-full text-[#996515]">
+      <PageBack/>
       <div className="flex flex-col justify-center items-center">
-        <div className="title mt-12 mb-9 px-12 py-1 bg-white rounded-3xl border-2 border-[#D2B48C]">
+        <div className="title mt-12 mb-9 px-12 py-1 bg-white rounded-3xl border-2 border-[#D2B48C] z-20">
           <p className="text-xl">대여 물품 LIST</p>
         </div>
-        <div className="itemslist font-Moneygraphy w-[90%]">
+        <div className="itemslist font-Moneygraphy w-[90%] z-20">
           <Item/>
         </div>
       </div> 
--- src/utils/images.jsx
+++ src/utils/images.jsx
@@ -7,4 +7,8 @@ export const images = {
   //rentalitems
   cushion : PATH + '/assets/images/rental/cushion.png',
   line : PATH + '/assets/images/rental/Line.png',
+  aBack : PATH + '/assets/images/rental/aBack.png',
+  eBack : PATH + '/assets/images/rental/eBack.png',
+  gBack : PATH + '/assets/images/rental/gBack.png',
+  pBack : PATH + '/assets/images/rental/pBack.png',
 };
