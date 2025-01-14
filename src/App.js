4ea838dfff98628c2e525bfd7d9588ae8499f88d
Koohyewon <khw6746@naver.com>
2025-01-14 23:18:04 +0900

    [fix] 리액트 아이콘 install

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './css/style.css';
-import Footer from './components/Footer';
+import "./css/style.css";
+import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
-import User from './routes/User';
+import User from "./routes/User";
 
 function App() {
   return (
     <div className="App">
       {/* Main Content */}
-        <Routes>
-          <Route path = "/*" element={<User/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/*" element={<User />} />
+      </Routes>
       {/* Footer */}
       <Footer />
     </div>
--- src/pages/User/ExcellentClub/RibbonBackground.jsx
+++ src/pages/User/ExcellentClub/RibbonBackground.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { images } from '../../../utils/images';
+import { images } from "../../../utils/images";
 
 export default function RibbonBackground() {
   return (
     <>
       {/* 리본 배경 */}
       <div className="flex absolute items-center justify-center h-full ">
-          <img src={images.ribbon} className="transform rotate-[-40deg]" />
+        <img src={images.ribbon} className="transform rotate-[-40deg]" />
       </div>
     </>
   );
