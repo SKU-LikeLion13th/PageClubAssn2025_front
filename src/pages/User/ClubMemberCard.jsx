52d410af3ce337cb0ab071c05e34388c4f8e8c43
Koohyewon <khw6746@naver.com>
2025-01-27 02:34:46 +0900

    [design] memberCard

--- src/pages/User/ClubMemberCard.jsx
+++ src/pages/User/ClubMemberCard.jsx
@@ -6,9 +6,10 @@ export default function ClubMemberCard() {
   return (
     <>
       <PageBackground />
-      <Header />
 
-      <div className="relative z-10 w-full h-[calc(100vh-95px)] flex flex-col items-center">
+      <div className="relative z-10 w-full h-[calc(100vh-35px)] flex flex-col items-center">
+        <Header />
+
         {/* 성결대 로고 */}
         <div className="mt-10">
           <img
@@ -21,7 +22,7 @@ export default function ClubMemberCard() {
         <div className="h-[45.22px] flex justify-center items-center bg-[#FFFFFF] border border-[#D2B48C] rounded-full px-3 my-12">
           <img
             src="assets/images/PageLogo.png"
-            alt="page 로고"
+            alt="pageLogo"
             className="h-[36.6px]"
           />
           <p className="text-2xl text-[#996515] pr-3">성결대학교 동아리원증</p>
@@ -29,11 +30,7 @@ export default function ClubMemberCard() {
         {/* 동아리원증 */}
         <div className="w-[90%] font-Moneygraphy text-center bg-[#FFFBFB] border border-[#D2B48C] rounded-2xl">
           <div className="min-h-[140px] flex items-center px-3">
-            <img
-              src="assets/images/likelion.png"
-              alt="동아리 로고"
-              className=""
-            />
+            <img src="assets/images/likelion.png" alt="clubLogo" className="" />
             <div className="text-[#996515] flex-1 flex flex-col justify-center items-center">
               <p className="text-lg">성결대 멋쟁이사자처럼 ▼</p>
               <p className="w-[90%] my-3 border-t-[0.1px] border-[#997C6E] flex items-center"></p>
