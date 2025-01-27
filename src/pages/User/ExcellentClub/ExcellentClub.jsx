f72057315e7aa592ab3186a54e71cbf5c5dbb1bf
cleame8922 <cleame8922@sungkyul.ac.kr>
2025-01-27 15:53:07 +0900

    [fix]리본 배경  사진 수정, ExcellentClub 수정

--- src/pages/User/ExcellentClub/ExcellentClub.jsx
+++ src/pages/User/ExcellentClub/ExcellentClub.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RibbonBackground from "../../../components/RibbonBackground";
 import Score from "./Score";
 import ScoreList from "./ScoreList";
 import Header from "../../../components/Header";
+import axios from "axios";
+import { API_URL } from "../../../config";
 
 export default function ExcellentClub() {
+  const [clubScore, setClubScore] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("Token");
+
+    axios
+    .get(`${API_URL}/club-scores/all`, {
+      headers: {
+        "Authorization" : `${token}`,
+        "Accept" : "*/*",
+      },
+    })
+    .then((response) => {
+      setClubScore(response.data);
+    })
+    .catch((error) => {
+      console.error("Error fetching joined clubs:", error);
+    })
+  }, []);
+
   return (
     <div className="relative w-full min-h-screen">
       <Header />
@@ -13,10 +35,10 @@ export default function ExcellentClub() {
       {/* 콘텐츠 */}
       <div className="z-10 flex flex-col items-center h-full">
         {/* TOP3 점수 */}
-        <Score />
+        <Score scores={clubScore} />
 
         {/* 순위 목록 */}
-        <ScoreList />
+        <ScoreList scores={clubScore} />
       </div>
     </div>
   );
--- src/pages/User/ExcellentClub/ScoreList.jsx
+++ src/pages/User/ExcellentClub/ScoreList.jsx
@@ -1,22 +1,7 @@
 import React from "react";
 import { images } from "../../../utils/images";
 
-export default function ScoreList() {
-  const scores = [
-    {
-      id: 1,
-      name: "성결대 멋쟁이사자처럼",
-      image: images.likelion,
-      score: 300,
-    },
-    { id: 2, name: "페가수스", image: images.pegasuss, score: 200 },
-    { id: 3, name: "애드마인", image: images.admin, score: 100 },
-  ];
-
-  const sortedScores = scores
-    .sort((a, b) => b.score - a.score)
-    .map((item, index) => ({ ...item, grade: index + 1 }));
-
+export default function ScoreList({clubScore = []}) {
   const getGrade = (grade) => {
     switch (grade) {
       case 1:
@@ -32,11 +17,11 @@ export default function ScoreList() {
 
   return (
     <div className="flex flex-col font-Moneygraphy w-[85%] bg-[#ffffff] border-[1px] border-[#d2b48c] text-[#996515] rounded-[10px] mt-10 z-10">
-      {sortedScores.map((item, index) => (
+      {clubScore.map((item, index) => (
         <div
           key={item.id}
           className={`flex px-3 py-4 ${
-            index !== sortedScores.length - 1
+            index !== clubScore.length - 1
               ? "border-b-[1px] border-[#d2b48c]"
               : ""
           }`}
