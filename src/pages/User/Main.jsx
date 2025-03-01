9269056621268c73f35dc273bb385d4e4ae7baad
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-03-01 17:33:08 +0900

    [fix]Main, Menu

--- src/pages/User/Main.jsx
+++ src/pages/User/Main.jsx
@@ -1,7 +1,8 @@
 import { NavLink } from "react-router-dom";
 import { images } from "../../utils/images";
-
+import "../../css/Main.css";
 import MainHeader from "../../components/MainHeader";
+import { DiJava } from "react-icons/di";
 
 export default function Main() {
   return (
@@ -11,28 +12,45 @@ export default function Main() {
       <div className="flex flex-col w-full mt-5">
         {" "}
         {/* 페이지로고 */}
-        <NavLink to='/' className="flex justify-center">
+        <NavLink to="/" className="flex justify-center">
           <img src={images.page} alt="" className="w-[260px]" />
         </NavLink>
         <div className="max-w-[500px]:w-full relative">
           {/* 책 배경 이미지 */}
           <img
-            className="max-w-[500px]:absolute max-w-[500px]:left-0 mt-[13%]"
+            className="max-w-[500px]:absolute max-w-[500px]:left-0 mt-[13%] w-[78%]"
             src="/assets/images/MainBook.png"
             alt="책 배경 이미지"
           />
+
           <NavLink to="/rentalitems">
             {/* absolute w-[200px] h-12 border-2 top-[30%] max-w-[500px]:left-[45%] transform -translate-x-[34%] z-10 */}
-            <button className="absolute w-[130px] h-12 top-[20%] max-w-[500px]:left-[30%] transform -translate-x-[65%] z-10"></button>
+            <button className="absolute w-[130px] h-12 top-[20%] max-w-[500px]:left-[30%] transform -translate-x-[65%] z-10 ">
+              <div className="text-[150%] pr-5 text-[#3f3f3f] flex justify-center items-center">
+                대여사업
+              </div>
+            </button>
           </NavLink>
           <NavLink to="/member-card">
-            <button className="absolute w-[130px] h-12 top-[33%] max-w-[500px]:left-[30%] transform -translate-x-[57%] z-10"></button>
+            <button className="absolute w-[130px] h-12 top-[33%] max-w-[500px]:left-[30%] transform -translate-x-[57%] z-10 ">
+              <div className="text-[150%] pr-5 text-[#3f3f3f] flex justify-center items-center">
+                동아리원증
+              </div>
+            </button>
           </NavLink>
           <NavLink to="/excellentclub">
-            <button className="absolute w-[170px] h-12 top-[46%] max-w-[500px]:left-[30%] transform -translate-x-[37%] z-10"></button>
+            <button className="absolute w-[170px] h-12 top-[46%] max-w-[500px]:left-[30%] transform -translate-x-[37%] z-10 ">
+              <div className="text-[150%] pr-5 text-[#3f3f3f] flex justify-center items-center">
+                활동 점수 Top3
+              </div>
+            </button>
           </NavLink>
           <NavLink to="/classSchedule">
-            <button className="absolute w-[140px] h-12 top-[58.5%] max-w-[500px]:left-[30%] transform -translate-x-[37%] z-10"></button>
+            <button className="absolute w-[140px] h-12 top-[58.5%] max-w-[500px]:left-[30%] transform -translate-x-[37%] z-10 ">
+              <div className="text-[150%] pr-5 text-[#3f3f3f] flex justify-center items-center">
+                빈 강의실
+              </div>
+            </button>
           </NavLink>
         </div>
       </div>
--- src/pages/User/Menu/Menu.jsx
+++ src/pages/User/Menu/Menu.jsx
@@ -12,18 +12,30 @@ export default function Menu() {
         <div className="max-w-[500px]:w-full relative mb-5">
           {/* 책 배경 이미지 */}
           <img
-            className="max-w-[500px]:absolute max-w-[500px]:left-0 mt-[10%]"
+            className="max-w-[500px]:absolute max-w-[500px]:left-0 mt-[10%] w-[78%]"
             src="/assets/images/MenuBook.png"
             alt="책 배경 이미지"
           />
           <NavLink to="/Introduction">
-            <button className="absolute w-[200px] h-12 top-[25%] max-w-[500px]:left-[45%] transform -translate-x-[34%] z-10"></button>
+            <button className="absolute w-[180px] h-[42px] top-[25.5%] max-w-[500px]:left-[45%] transform -translate-x-[40%] z-10 ">
+              <div className="text-[150%] pr-9 text-[#3f3f3f] flex justify-center items-center">
+                동아리연합회 소개
+              </div>
+            </button>
           </NavLink>
           <NavLink to="/SNS">
-            <button className="absolute w-36 h-[47px] top-[38%] max-w-[500px]:left-[50%] transform -translate-x-[40%] z-10"></button>
+            <button className="absolute w-32 h-[40px] top-[38.5%] max-w-[500px]:left-[50%] transform -translate-x-[48%] z-10 ">
+              <div className="text-[150%] pr-8 text-[#3f3f3f] flex justify-center items-center">
+                SNS
+              </div>
+            </button>
           </NavLink>
           <NavLink to="/AboutUs">
-            <button className="absolute w-[140px] h-[47px] top-[50%] max-w-[500px]:left-[30%] transform -translate-x-[30%] z-10"></button>
+            <button className="absolute w-[140px] h-[40px] top-[51%] max-w-[500px]:left-[30%] transform -translate-x-[36%] z-10 ">
+              <div className="text-[150%] pr-12 text-[#3f3f3f] flex justify-center items-center">
+                만든이들
+              </div>
+            </button>
           </NavLink>
         </div>
       </div>
