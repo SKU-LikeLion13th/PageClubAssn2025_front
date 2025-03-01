65c04f4bc4f6524a61640888965bd41e874ed3a5
cleame8922 <cleame8922@sungkyul.ac.kr>
2025-03-01 22:12:58 +0900

    [fix]반응형 수정

--- src/pages/User/ExcellentClub/ExcellentClub.jsx
+++ src/pages/User/ExcellentClub/ExcellentClub.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { images } from "../../../utils/images";
 import Score from "./Score";
 import ScoreList from "./ScoreList";
@@ -6,11 +6,13 @@ import Header from "../../../components/Header";
 import axios from "axios";
 import { API_URL } from "../../../config";
 import Loading from "../../../components/Loading";
-import "../../../css/ExcellentClub.css";
+import '../../../css/ExcellentClub.css';
 
 export default function ExcellentClub() {
   const [clubScore, setClubScore] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentIndexes, setCurrentIndexes] = useState({});
+  const intervalIds = useRef({});
 
   useEffect(() => {
     const token = localStorage.getItem("Token");
@@ -18,7 +20,7 @@ export default function ExcellentClub() {
     setIsLoading(true);
 
     axios
-      .get(`${API_URL}/club-scores/all`, {
+      .get(`${API_URL}/club-scores/ranked`, {
         headers: {
           Authorization: `${token}`,
           Accept: "*/*",
@@ -41,21 +43,46 @@ export default function ExcellentClub() {
         setIsLoading(false);
       });
   }, []);
-  
+
+  useEffect(() => {
+    clubScore.forEach((item) => {
+      const sameRankTeams = clubScore.filter((team) => team.ranking === item.ranking);
+
+      if (sameRankTeams.length > 1 && !intervalIds.current[item.ranking]) {
+        intervalIds.current[item.ranking] = setInterval(() => {
+          setCurrentIndexes((prevIndexes) => {
+            const currentIndex = prevIndexes[item.ranking] || 0;
+            return {
+              ...prevIndexes,
+              [item.ranking]: (currentIndex + 1) % sameRankTeams.length,
+            };
+          });
+        }, 2500);
+      }
+    });
+
+    clubScore.forEach((item) => {
+      const sameRankTeams = clubScore.filter((team) => team.ranking === item.ranking);
+      if (sameRankTeams.length <= 1 && intervalIds.current[item.ranking]) {
+        clearInterval(intervalIds.current[item.ranking]);
+        intervalIds.current[item.ranking] = null;
+      }
+    });
+
+  }, [clubScore]);
+
   return (
-    <div className="relative min-h-screen">
+    <div className="relative min-h-[calc(100vh-200px)]">
       <Header />
 
-      {/* 활동점수 Top 3 제목 */}
-      <div className="z-10">
+      <div className="z-20">
         <div className="flex justify-center w-fit mx-auto text-[25.5px] text-[#996515] border-[0.5px] border-[#996515] bg-[#ffffff] rounded-[62px] mt-8 px-11 py-1">
-          {/* {clubScore.length > 0 ? clubScore[0].quarter : ""}  */}
-          활동 점수 TOP3
+          {clubScore.length > 0 ? clubScore[0].quarter : ""} 활동 점수 TOP3
         </div>
 
         <div className="flex items-center justify-center w-full">
           {isLoading ? (
-            <div className="flex flex-col items-center top-1/2">
+            <div className="flex flex-col items-center w-full loading">
               <Loading />
             </div>
           ) : clubScore.length > 0 ? (
@@ -64,7 +91,7 @@ export default function ExcellentClub() {
               <ScoreList scores={clubScore} currentIndexes={currentIndexes} />
             </div>
           ) : (
-            <div className="flex items-center justify-center w-9/12 ready">
+            <div className="z-10 flex items-center justify-center w-9/12 mt-[4rem]">
               <div className="w-full h-[22rem] flex items-center justify-center font-Moneygraphy text-[15px] bg-[#ffffff] border-[1px] border-[#D2B48C] rounded-[13px] text-[#996515]">
                 Comming Soon!
               </div>
@@ -78,5 +105,5 @@ export default function ExcellentClub() {
         <img src={images.ribbon} className="w-full opacity-50" alt="ribbon" />
       </div>
     </div>
-  );  
+  );
 }
